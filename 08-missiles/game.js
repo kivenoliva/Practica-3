68c05e156248f6e3f510d3f3020d706a8a415f7f
Kevin Oliva Muñoz <kivenoliva@gmail.com>
2014-10-25 17:22:54 +0200

    Protocolo 08. Terminado ejercicio 5

--- 08-missiles/game.js
+++ 08-missiles/game.js
@@ -95,6 +95,7 @@ var Starfield = function(speed,opacity,numStars,clear) {
 // La clase PlayerShip tambien ofrece la interfaz step(), draw() para
 // poder ser dibujada desde el bucle principal del juego
 var PlayerShip = function() { 
+    var up = false;
     this.w =  SpriteSheet.map['ship'].w;
     this.h =  SpriteSheet.map['ship'].h;
     this.x = Game.width/2 - this.w / 2;
@@ -118,10 +119,13 @@ var PlayerShip = function() {
 	    this.x = Game.width - this.w 
 	}
 
+     
 	this.reload-=dt;
-	if(Game.keys['fire'] && this.reload < 0) {
+		
+	if(!Game.keys['fire']) up = true;
+	if(up && Game.keys['fire'] && this.reload < 0) {
 	    // Esta pulsada la tecla de disparo y ya ha pasado el tiempo reload
-	    Game.keys['fire'] = false;
+	    up = false;
 	    this.reload = this.reloadTime;
 
 	    // Se añaden al gameboard 2 misiles 
